362ad82e6292e16aa10040cd2969d86a152d0355
hotaru <36354847+hotaru51@users.noreply.github.com>
2021-05-17 23:45:27 +0900

    デプロイスクリプト更新

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 set -ue
 
-pwd_dir=$(cd $(dirname $0); pwd)
-backup_dir=${pwd_dir}/old
+work_dir=$(cd $(dirname $0); pwd)
+backup_dir=${work_dir}/old
+config_dir=${HOME}/.config
+
+dotfiles=(
+    ${config_dir}/starship.toml
+)
 
 test ! -d ${backup_dir} && mkdir ${backup_dir}
+test ! -d ${config_dir} && mkdir ${config_dir}
 
 # 初期の.bashrcをバックアップ
 if [ -f ${HOME}/.bashrc ]; then
@@ -13,11 +19,22 @@ if [ -f ${HOME}/.bashrc ]; then
 fi
 
 # シンボリックリンク作成
-ln -s ${pwd_dir}/.mybashrc ${HOME}/.mybashrc
+ln -s ${work_dir}/.mybashrc ${HOME}/.mybashrc
 
-# デフォルトの.bashrcにincludeする設定追記
+デフォルトの.bashrcにincludeする設定追記
 cat <<'_EOT_' >> ${HOME}/.bashrc
 if [ -f ${HOME}/.mybashrc ]; then
     . ${HOME}/.mybashrc
 fi
 _EOT_
+
+for file in "${dotfiles[@]}"
+do
+    # dotfileが存在する場合バックアップ
+    if [ -f "${file}" ]; then
+        mv ${file} ${backup_dir}
+    fi
+
+    # シンボリックリンクを作成
+    ln -s ${work_dir}/$(basename ${file}) ${file}
+done
